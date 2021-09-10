fb88af4ec30b478624e92844482db2c13e1255a6
JoaoGabryelW <90428603+JoaoGabryelW@users.noreply.github.com>
2021-09-09 21:31:03 -0300

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,15 @@
+section .data
+  msg db "Hello world!", Oah
+
+section .text
+  global _start
+
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, 13
+  syscall
+  mov rax, 60
+  mov rdi, 0
+  syscall
